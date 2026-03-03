a8e4cea34cfe49770fb3f10510b0aeebf0653d04
Caleb Karimi <karimicaleb@gmail.com>
2026-03-03 21:40:41 +0300

    Fix the private IP restriction issue for local dev in relation to Next.js Image Optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    // This line to bypass the private IP restriction for local dev
+    unoptimized: process.env.NODE_ENV === 'development',
     remotePatterns: [
       {
         protocol: 'http',
